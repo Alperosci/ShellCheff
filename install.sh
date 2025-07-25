7940d665f09b4dc718f9ac12dea252674cd6be2a
Alperosci <alperosci@gmail.com>
2025-07-25 16:58:41 +0300

    Added main file and install file

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_NAME="ShellCheff"
+INSTALL_PATH="/usr/local/bin"
+
+echo "[*] Making script executable..."
+chmod +x $SCRIPT_NAME
+
+echo "[*] Copying script to $INSTALL_PATH ..."
+sudo cp $SCRIPT_NAME $INSTALL_PATH/
+
+echo "[*] Setting execute permission..."
+sudo chmod +x $INSTALL_PATH/$SCRIPT_NAME
+
+# Python ve pip3 kontrolü
+if ! command -v python3 &> /dev/null
+then
+    echo "[!] Python3 is not installed. Please install Python3 first."
+    exit 1
+fi
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "[!] pip3 is not installed. Installing pip3..."
+    sudo apt update && sudo apt install -y python3-pip
+fi
+
+echo "[*] Installing required Python packages..."
+sudo pip3 install --user art --break-system-packages
+
+echo "Installation Complete! You can write '$SCRIPT_NAME' to terminal to use it."
